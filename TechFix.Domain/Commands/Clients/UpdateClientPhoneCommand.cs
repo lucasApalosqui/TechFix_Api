799c5d0e8d63c3a2ec77d94c05cfa4698fde9a85
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-05-21 19:01:32 -0300

    :sparkles: feat: Client Commands

---
+++ TechFix.Domain/Commands/Clients/UpdateClientPhoneCommand.cs
@@ -0,0 +1,34 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using TechFix.Domain.Commands.Contracts;
+using TechFix.Domain.ValueObjects;
+
+namespace TechFix.Domain.Commands.Clients
+{
+    public class UpdateClientPhoneCommand : Notifiable, ICommand
+    {
+        public UpdateClientPhoneCommand() { }
+
+        public UpdateClientPhoneCommand(string phone, Guid id)
+        {
+            Phone phoneVerify = new Phone(phone);
+            if (phoneVerify.Invalid)
+                AddNotification(phone, phoneVerify.Notifications.ToString());
+
+            Id = id;
+            Phone = phone;
+        }
+
+        public Guid Id { get; set; }
+        public string Phone { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .IsNotNull(Id, "Id", "CLiente inválido!")
+                );
+        }
+    }
+}
---
+++ TechFix.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechFix.Domain.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+    }
+}
