f81f3c3fde695d9bd659645debe980253eca3d81
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-05-22 16:42:25 -0300

    :sparkles: feat: Client Handlers Test

---
+++ TechFix.Domain.Tests/Handlers/ClientHandlers/CreateClientHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechFix.Domain.Commands;
+using TechFix.Domain.Commands.Clients;
+using TechFix.Domain.Handlers.ClientHandlers;
+using TechFix.Domain.Tests.Repositories;
+
+namespace TechFix.Domain.Tests.Handlers.ClientHandlers
+{
+    [TestClass]
+    public class CreateClientHandlerTests
+    {
+        private readonly CreateClientCommand _invalidCommand = new CreateClientCommand("lucas", "apalosqui", "lucasemail.com", "12345678");
+        private readonly CreateClientCommand _validCommand = new CreateClientCommand("lucas", "apalosqui", "lucas@email.com", "12345678");
+        private GenericCommandResult _genericCommandResult = new GenericCommandResult();
+        private readonly ClientHandler _clientHandler = new ClientHandler(new FakeClientRepository());
+
+        [TestMethod]
+        public void Dado_um_comando_invalido_devera_parar_a_execucao()
+        {
+            _genericCommandResult = (GenericCommandResult)_clientHandler.Handle(_invalidCommand);
+            Assert.AreEqual(_genericCommandResult.Success, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_cliente_devera_ser_criado()
+        {
+            _genericCommandResult = (GenericCommandResult)_clientHandler.Handle(_validCommand);
+            Assert.AreEqual(_genericCommandResult.Success, true);
+        }
+    }
+}
---
+++ TechFix.Domain.Tests/Repositories/FakeClientRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechFix.Domain.Entities;
+using TechFix.Domain.Repositories;
+
+namespace TechFix.Domain.Tests.Repositories
+{
+    public class FakeClientRepository : IClientRepository
+    {
+        public void Create(ClientEntity client)
+        {
+            
+        }
+
+        public ClientEntity GetById(Guid id)
+        {
+            return new ClientEntity("lucas", "apalosqui", "lucas@email.com", "12345678");
+        }
+
+        public void Update(ClientEntity client)
+        {
+            
+        }
+    }
+}
